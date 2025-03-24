c5c0700f6c9ec7ad1d6b449cc884af190da9f8c1
sebas-serrano <serranoavila@gmail.com>
2025-03-24 07:15:56 -0300

    primer comit del proyecto de estudio del curso de analitica e ia en la UTEC

---
+++ seccion1.1.py
@@ -0,0 +1,37 @@
+print('¡Hola Mundo!')
+print('Hola', 'Sebas')
+
+# Booleano (bool)
+tienes_mascotas = False
+print(tienes_mascotas)
+print(type(tienes_mascotas))
+
+# Enteros (int)
+edad = 28
+print(edad)
+print(type(edad))
+
+# Flotantes (float)
+precio = 199.99
+print(precio)
+print(type(precio))
+
+# Texto (str)
+nombre = 'Rosa 🌹'
+print(nombre)
+print(type(nombre))
+
+# Pedir un texto al usuario
+nombre_mascota = input('¿Cuál es el nombre de tu mascota?')
+print(nombre_mascota)
+
+# Pedir un entero al usuario
+numero = int(input('Dame un número entero: '))
+print(numero)
+
+# Pedir un flotante al usuario
+decimal = float(input('Dame un número decimal: '))
+print(decimal)
+
+print((10 - 2) * (3 + 2) ** 2 / 2)
+print((5 + 3) * 2)
