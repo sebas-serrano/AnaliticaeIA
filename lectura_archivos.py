a2f4ffe434409ea617997e3130498b0ee5f2e537
sebas-serrano <serranoavila@gmail.com>
2025-03-27 20:45:32 -0300

    ejercicios varios - lectura archivos

---
+++ lectura_archivos.py
@@ -0,0 +1,13 @@
+
+# Abrir un archivo con open() y close()
+file_handler = open('c:/planets.csv')
+
+for linea in file_handler:
+    print(linea)
+
+file_handler.close()
+
+# Abrir un archivo con with
+with open('c:/planets.csv') as archivo:
+    for linea in archivo:
+        print(linea)
