987c19653b8f3233c2ff6fe66768e0f36d9d0d7a
sebas-serrano <serranoavila@gmail.com>
2025-04-13 23:48:37 -0300

    primeros pasos con nunpy y pandas

--- modulo1/subtema-1.5/ejerciciosClase.py
+++ modulo1/subtema-1.5/ejerciciosClase.py
@@ -1,32 +1,45 @@
 import numpy as np
 
-fila = int(input("Ingrese la cantidad de filas de la matriz: "))
-columna = int(input("Ingrese la cantidad de columnas de la matriz: "))
+arreglo1 = 0
+arreglo2 = 0
 
-print("La primer matriz es de: ", fila, " x ", columna)
-print("La segunda matriz es de: ", columna, " x ", fila)
+def ingresoDatos():
 
-# Crear matriz vacía
-arreglo1 = np.zeros((fila, columna), dtype=int)
+    global arreglo1, arreglo2
 
-# Cargar datos
-print("Ingrese los valores de la primera matriz:")
-for i in range(fila):
-    for j in range(columna):
-        valor = int(input(f"Ingrese el valor para la posición [{i}][{j}]: "))
-        arreglo1[i][j] = valor
+    fila = int(input("Ingrese la cantidad de filas de la matriz: "))
+    columna = int(input("Ingrese la cantidad de columnas de la matriz: "))
 
-# Crear segunda matriz para multiplicar
-arreglo2 = np.zeros((columna, fila), dtype=int)
-print("Ingrese los valores de la segunda matriz:")
-for i in range(columna):
-    for j in range(fila):
-        valor = int(input(f"Ingrese el valor para la posición [{i}][{j}]: "))
-        arreglo2[i][j] = valor
+    print("La primer matriz es de: ", fila, " x ", columna)
+    print("La segunda matriz es de: ", columna, " x ", fila)
 
-# Multiplicar matrices
-resultado = np.dot(arreglo1, arreglo2)
+    # Crear matriz vacía
+    arreglo1 = np.zeros((fila, columna), dtype=int)
 
-# Mostrar resultado
-print("Resultado de la multiplicación:")
-print(resultado)
+    # Cargar datos
+    print("Ingrese los valores de la primera matriz:")
+    for i in range(fila):
+        for j in range(columna):
+            valor = int(input(f"Ingrese el valor para la posición [{i}][{j}]: "))
+            arreglo1[i][j] = valor
+
+    # Crear segunda matriz para multiplicar
+    arreglo2 = np.zeros((columna, fila), dtype=int)
+    print("Ingrese los valores de la segunda matriz:")
+    for i in range(columna):
+        for j in range(fila):
+            valor = int(input(f"Ingrese el valor para la posición [{i}][{j}]: "))
+            arreglo2[i][j] = valor
+
+def multiplicar():
+    global arreglo1, arreglo2
+
+    # Multiplicar matrices
+    resultado = np.dot(arreglo1, arreglo2)
+
+    # Mostrar resultado
+    print("Resultado de la multiplicación:")
+    print(resultado)
+
+ingresoDatos()
+multiplicar()
