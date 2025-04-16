3040dd91bd4a425ca24be5d56f2f2e92ed29a936
sebas-serrano <serranoavila@gmail.com>
2025-04-16 16:06:38 -0300

    ejercicios de numpy de clase

--- modulo1/subtema-1.5/ejercicio2Clase(numpy).py
+++ modulo1/subtema-1.5/ejercicio2Clase(numpy).py
@@ -1,5 +1,17 @@
 import numpy as np
 
+"""
+En una línea de manufactura se fabrican dos tipos de focos: transparentes (T) y opacos (O). 
+De cada tipo se hacen cuatro modelos M1, M2, M3 y M4. Se debe pedir al operador del programa 
+que introduzca la cantidad de focos que se realizaron de cada tipo y modelo. 
+Considerando que el porcentaje de focos defectuosos del 2% para el modelo 1, el 5% para el modelo 2, 
+el 8% para el modelo 3 y 10% para el modelo 4, 
+calcule lo siguiente: 
+    a) La cantidad de focos transparentes en buen estado que se fabrican 
+    b) La cantidad de focos opacos defectuosos que se fabrican 
+    c) Cuál es el tipo y modelo de foco que más se produce en mal estado
+"""
+
 # Crear matriz vacía
 defectuoso = [0.02, 0.05, 0.08, 0.1]
 tipos = ['Transparentes','Opacos']
@@ -7,6 +19,15 @@ tipos = ['Transparentes','Opacos']
 datos = np.zeros((2, 4), dtype=int)
 
 def cargarDatos():
+    """
+        Este metodo carga los datos del usuario.
+
+        Args:
+            No tiene entradas.
+        
+        Returns:
+            Retorna la matriz con los datos cargados.    
+    """
     global datos
     # Cargar datos
     for i in range(2):
