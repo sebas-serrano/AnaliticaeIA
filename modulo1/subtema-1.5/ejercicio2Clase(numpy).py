d6c377a86972e0b3800498f19f32da98253284bf
sebas-serrano <serranoavila@gmail.com>
2025-04-16 23:35:12 -0300

    ejercicios de numpy de clase

--- modulo1/subtema-1.5/ejercicio2Clase(numpy).py
+++ modulo1/subtema-1.5/ejercicio2Clase(numpy).py
@@ -15,8 +15,9 @@ calcule lo siguiente:
 # Crear matriz vacía
 defectuoso = [0.02, 0.05, 0.08, 0.1]
 tipos = ['Transparentes','Opacos']
+modelos = ['M1','M2','M3','M4']
 
-datos = np.zeros((2, 4), dtype=int)
+datos = np.zeros((2, 4), dtype=float)
 
 def cargarDatos():
     """
@@ -31,12 +32,59 @@ def cargarDatos():
     global datos
     # Cargar datos
     for i in range(2):
-        print("Ingrese los valores del tipo: ", tipos[i])
+        print("Ingrese la cantidad del tipo: ", tipos[i])
         for j in range(4):
-            valor = int(input(f"Ingrese el valor para la posición [{i}][{j}]: "))
+            valor = int(input(f"Ingrese la cantida de focos para el tipo {modelos [j]} [{i}][{j}]: "))
             datos[i][j] = valor
 
+def ejercicioA():
+    """
+        Funcion: La cantidad de focos transparentes en buen estado que se fabrican.
+
+        Args:
+            La matriz con los datos cargados.
+        
+        Returns:
+            Cantidad de Focos transparentes en buen estado.
+        
+    """
+    global datos, defectuoso
+    salida = np.zeros(4, dtype=float)
+    for i in range (4):
+        salida[i] = datos[0, i] - (defectuoso[i] * datos[0, i])
+    return salida
+
+def ejercicioB():
+    """
+    Funcion: La cantidad de focos opacos defectuosos que se fabrican
+
+    Args:
+        La matriz con los datos cargados.
+    
+    Returns:
+        Cantidad de Focos opacos defectuosos.
+    
+    """
+    global datos, defectuoso
+    salida = np.zeros(4, dtype=float)
+    for i in range (4):
+        salida[i] = defectuoso[i] * datos[1, i]
+    return salida
+
+# Main principal
 cargarDatos()
-print(datos[1,2])
+
+#Llamamos al primer ejercicio e imprimos el resultado
+salidaA =ejercicioA()
+print("Cantidad de Focos Transparentes en buen estado:")
+for i in range(4):
+    print("Del modelo ", modelos[i], " se fabricaron ", salidaA[i])
+
+#Llamamos al segundo ejercicio e imprimos el resultado
+salidaB =ejercicioB()
+print("Cantidad de Focos Opacos defectuosos:")
+for i in range(4):
+    print("Del modelo ", modelos[i], " se fabricaron ", salidaB[i])
+
 
         
