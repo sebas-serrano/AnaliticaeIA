fac9e920222a190923e94e6e6871eca9f2ceea90
sebas-serrano <serranoavila@gmail.com>
2025-04-26 12:24:23 -0300

    practica de numpy

---
+++ practicaParalela/numpyEjercicios2.py
@@ -0,0 +1,33 @@
+"""
+Atributos de un array
+Existen varios atributos y funciones que describen las características de un array.
+
+"""
+
+import numpy as np
+a1 = np.ones(4)
+print(a1)
+a2 = np.ones([4,3])
+print(a2)
+a3 = np.ones([4,3,8])
+print(a3)
+
+# a.ndim : Devuelve el número de dimensiones del array a.
+print(a1.ndim)
+print(a2.ndim)
+print(a3.ndim)
+
+# a.shape : Devuelve una tupla con las dimensiones del array a.
+print(a1.shape)
+print(a2.shape)
+print(a3.shape)
+
+# a.size : Devuelve el número de elementos del array a.
+print(a1.size)
+print(a2.size)
+print(a3.size)
+
+# a.dtype: Devuelve el tipo de datos de los elementos del array a.
+print(a1.dtype)
+print(a2.dtype)
+print(a3.dtype)
