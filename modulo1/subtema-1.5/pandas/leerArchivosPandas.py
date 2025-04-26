b20a0d1d96b76ba82a68a1f205556e7ade6ecabe
sebas-serrano <serranoavila@gmail.com>
2025-04-26 18:29:32 -0300

    ejercicios de pandas

--- modulo1/subtema-1.5/pandas/leerArchivosPandas.py
+++ modulo1/subtema-1.5/pandas/leerArchivosPandas.py
@@ -3,4 +3,13 @@ import pandas as pd
 # Cargamos un archivo con pandas
 df = pd.read_csv("archivos/03 characters.csv")
 
-print(df)
+#print(df.head())
+print(df.info())
+
+#print(df.columns)
+print(df ['height'].min())
+
+a = df [['species','height']]
+print(a)
+b= a.groupby('species').sum().sort_values('height', ascending=True)
+print(b)
