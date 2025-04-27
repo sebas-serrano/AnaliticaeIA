581ff18762de8e14522005ce4053998fca82ac6f
sebas-serrano <serranoavila@gmail.com>
2025-04-27 09:05:11 -0300

    ejercicios de pandas

--- modulo1/subtema-1.5/pandas/startWars.py
+++ modulo1/subtema-1.5/pandas/startWars.py
@@ -3,23 +3,29 @@ import pandas as pd
 # Realiza la lectura del archivo starships.csv
 df = pd.read_csv("archivos/04 starships.csv")
 
+
 # Muestra la estructura del dataframe que contiene los datos
-print(df.info())
+# print(df.info())
+
 
 # Imprimos primeros registros
-print(df.head())
+# print(df.head())
+
 
 # ¿Cuál es el costo total de todas las naves que aparecen en el archivo de registro?
 total = df['cost_in_credits'].sum() 
 # print(total)
 
+
 # ¿Cuál es el promedio de costos por fabricante?
 promedio = df.groupby('manufacturer')['cost_in_credits'].mean()
 # print(promedio)
 
+
 # ¿Cuántas “starship_class” existen y cuáles son?
 cantidad_clases = df['starship_class'].nunique()
 # print("Existen", cantidad_clases, "starship_class distintas")
+
 
 clases_unicas = df['starship_class'].unique()
 # print("Las clases de starship_class son:", clases_unicas)
@@ -28,6 +34,7 @@ clases_unicas = df['starship_class'].unique()
 # ¿Cuál es la nave más barata?
 fila_menor_costo = df.sort_values('cost_in_credits', ascending=True).head(1)
 # print(fila_menor_costo)
+
 
 idx = df['cost_in_credits'].idxmin()
 nave_mas_barata = df.loc[idx]
@@ -39,9 +46,11 @@ idx_Larga = df['length'].idxmin()
 nave_mas_larga = df.loc[idx_Larga]
 # print(nave_mas_larga)
 
+
 # Obtén los registros de las naves con más de 1000 tripulantes (cargo_capacity)
-registros = df['cargo_capacity'] > 1000
-print(registros)
+mayores = df[df['cargo_capacity'] > 100000]
+print(mayores)
+
 
 
 
