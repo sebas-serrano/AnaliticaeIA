917c93f094db721ff0bd4d64e772deddb8e75773
sebas-serrano <serranoavila@gmail.com>
2025-05-01 15:23:27 -0300

    ejercicios con datos en python

---
+++ modulo1/subtema-1.6/manipulacionDatosPython.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df1 = pd.DataFrame(
+     {
+         "Nombre": ["Ana", "Pepe", "Hugo", "Maria"],
+         "Apellido": ["Sanchez", "Lopez", "Hernandez", "Gonzalez"],
+         "edad": ["26", "31", "9", "22"],
+         "puesto": ["junior dev", "Senior dev", "Practicante", "PM"],
+     },
+ )
+
+print(df1)
+
+
+
+
